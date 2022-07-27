9c8bdc2d200ee3b39455a8a4bb8223a8d7ed8147
Judith van Leersum <judithvleersum@gmail.com>
2022-07-27 17:53:34 +0200

    Fetching data from db on all meetings page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+import MeetupItem from "../components/meetups/MeetupItem";
 import MeetUpList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -23,22 +25,29 @@ const HomePage = (props) => {
   return <MeetUpList meetups={props.meetups} />;
 };
 
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-
-//   // fetch data from an API
-
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//   };
-// }
-
 export async function getStaticProps() {
   // fetch data from an API
-  return { props: { meetups: DUMMY_MEETUPS }, revalidate: 10 };
+  const client = await MongoClient.connect(
+    "mongodb+srv://development:development@cluster0.h9yhx.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
