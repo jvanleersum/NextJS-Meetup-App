b8d407e4a090cda5a23f050d1482483401bc9755
Judith van Leersum <judithvleersum@gmail.com>
2022-07-27 18:16:43 +0200

    Added head metadata to new meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
@@ -9,19 +11,30 @@ const NewMeetupPage = () => {
       method: "POST",
       body: JSON.stringify(enteredMeetupData),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
     const data = await response.json();
     console.log(data);
 
-    if (data.message === 'Meetup inserted') {
-      router.replace('/');
+    if (data.message === "Meetup inserted") {
+      router.replace("/");
     }
   };
 
-  return <NewMeetupForm onAddMeetup={addMeetuphandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new meetiup</title>
+        <meta
+          name="description"
+          content="Add a new React meetup!"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetuphandler} />
+    </Fragment>
+  );
 };
 
 export default NewMeetupPage;
