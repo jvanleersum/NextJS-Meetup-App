b6e752b208b64e8424e8ad30a3bb235e1adfc9c4
Judith van Leersum <judithvleersum@gmail.com>
2022-07-27 16:06:27 +0200

    Added details page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetail
+      title="Meetup in Amsterdam"
+      image="https://images.unsplash.com/photo-1612911854559-2123745ab97c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      address="Amsterdamsestraat 1, 1000AA, Amsterdam"
+      description="This is the first meetup"
+    />
+  );
+};
+
+export default MeetupDetailsPage;
