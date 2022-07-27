d7a2a6e9714f6a836be4ce8441fb81f2b0a0504e
Judith van Leersum <judithvleersum@gmail.com>
2022-07-27 17:28:50 +0200

    Added functionality to insert data into the meetups collection on MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,21 @@
+import { MongoClient } from 'mongodb';
 // /api/new-meetup
 // POST /api/new-meetup
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
-    const { title, image, address, description } = data;
     
-    
+    const client = MongoClient.connect('mongodb+srv://development:development@cluster0.h9yhx.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db = (await client).db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    (await client).close();
+
+    res.status(201).json({message: 'Meetup inserted'})
   } 
 
 }
