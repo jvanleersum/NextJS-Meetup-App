e07ff3ad7608653f3025f9d9ffe4f07aee91c46e
Judith van Leersum <judithvleersum@gmail.com>
2022-07-27 18:13:31 +0200

    Add head metadata

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,18 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import { MongoClient } from "mongodb";
-import MeetupItem from "../components/meetups/MeetupItem";
 import MeetUpList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "Meetup in Amsterdam",
-    image:
-      "https://images.unsplash.com/photo-1612911854559-2123745ab97c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    address: "Amsterdamsestraat 1, 1000AA, Amsterdam",
-    description: "This is the first meetup",
-  },
-  {
-    id: "m2",
-    title: "Meetup again",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Amsterdamsestraat 1, 1000AA, Amsterdam",
-    description: "This is the second meetup",
-  },
-];
-
 const HomePage = (props) => {
-  return <MeetUpList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React meetups</title>
+        <meta name='description' content="Browse a huge list of highly active React meetups!" />
+      </Head>
+      <MeetUpList meetups={props.meetups} />
+    </Fragment>
+  );
 };
 
 export async function getStaticProps() {
